7c37e4f96f26549bfeb4b1b1edd7a6095a84bbdf
iiingest-github <76582610+iiingest-github@users.noreply.github.com>
2024-08-18 18:41:49 +0800

    Add files via upload

---
+++ pages/src/script.js
@@ -0,0 +1,35 @@
+﻿function openpage(pname){
+	document.getElementsByTagName("iframe")[0].src = "pages/" + pname + ".html";
+}
+/*function init(){
+	var url = window.location + "";
+	var mark = url.split("#");
+	if(mark.length==2){
+		var tr = document.getElementById(mark[1]);
+		tr.className = "marked";
+		//alert(tr.offsetTop - window.innerHeight/2);
+		window.scrollTo(0,tr.offsetTop - window.innerHeight/4);
+		var to = setTimeout(function(){tr.className = "";},1000);
+	}
+}*/
+function tomark(id){
+	var mark = document.getElementById(id);
+	mark.className = "marked";
+	var map = document.getElementsByTagName("table")[0];
+	if(map.offsetTop == 0){
+		map = document.getElementsByTagName("table")[1];
+	}
+	window.scrollTo(window.pageXOffset,mark.offsetTop + map.clientHeight);
+	var t = setTimeout(function(){mark.className = "";},500);
+}
+function tomark2(id){
+	var mark = document.getElementById(id);
+	mark.className = "marked";
+	var map = document.getElementsByTagName("table")[0];
+	window.scrollTo(window.pageXOffset,mark.parentNode.parentNode.offsetTop + map.clientHeight);
+	var t = setTimeout(function(){mark.className = "";},500);
+}
+
+function totop(){
+	$("html,body").animate({scrollTop:0},300);
+}
